68703d62ac014be1583256dcded91a2fb7e0a13c
ufaber-amit <amit.t@ufaber.com>
2020-09-01 20:03:02 +0530

    refactoring code

--- content/views.py
+++ content/views.py
@@ -6,11 +6,6 @@ from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-
-
-
-
-
 from .models import Content
 from .serializers import ContentSerializer
 
